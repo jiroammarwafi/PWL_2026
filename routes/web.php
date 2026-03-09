6421647eeb6e2f1b36606585c7b92ca91ea66a14
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-09 22:39:09 +0700

    Praktikum 1: 7/18

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('Selamat Datang');
+});
+
+Route::get ('/hello', function () {
+    return 'Hello world';
+});
+
+Route::get ('/world', function () {
+    return 'World';
+});
+
+Route::get ('/about', function () {
+    return 'NIM: 244107020190, Nama: Jiro Ammar Wafi';
 });

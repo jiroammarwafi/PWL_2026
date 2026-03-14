8ecd5c415880ccc579d4ccbec3e196aae3c60c93
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-15 00:00:00 +0700

    Jobsheet  2 - Praktikum 1 - 18/18

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,19 @@ Route::get ('/world', function () {
 Route::get ('/about', function () {
     return 'NIM: 244107020190, Nama: Jiro Ammar Wafi';
 });
+
+Route::get ('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get ('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post ke: ' . $postId . ', Comment ke: ' . $commentId;
+});
+
+Route::get ('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan id: ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/user/{name?}', function ($name = 'John') {
+return 'Nama saya '.$name;
+});

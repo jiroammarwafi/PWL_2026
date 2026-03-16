0a26954c089ea913c9e43b3e19a5ab880a0b812c
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-16 23:04:28 +0700

    Jobsheet 2/Praktikum 2/Langkah (7/11)

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function about()
+    {
+        return 'Nama: Jiro Ammar Wafi <br>NIM: 244107020190 <br>(Dari AboutController)';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get ('/hello', [WelcomeController::class, 'hello']);
 
@@ -12,7 +14,7 @@ Route::get ('/world', function () {
     return 'World';
 });
 
-Route::get ('/about', [PageController::class, 'about']);
+Route::get ('/about', [AboutController::class, 'about']);
 
 Route::get ('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
@@ -22,7 +24,7 @@ Route::get ('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Post ke: ' . $postId . ', Comment ke: ' . $commentId;
 });
 
-Route::get ('/articles/{id}', [PageController::class, 'articles'])->where('id', '[0-9]+');
+Route::get ('/articles/{id}', [ArticleController::class, 'articles'])->where('id', '[0-9]+');
 
 Route::get('/user/{name?}', function ($name = 'John') {
 return 'Nama saya '.$name;

9e2a790b466d775ca9d0def9a224b75b0a0d423a
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-17 21:03:23 +0700

    Jobsheet 2/Praktikum 3/Langkah (7/13)

--- routes/web.php
+++ routes/web.php
@@ -45,5 +45,5 @@ Route::resource('photos', PhotoController::class)->except([
 ]);
 
 Route::get('/greeting', function () {
-    return view('hello', ['name' => 'Jiro']);
+    return view('blog.hello', ['name' => 'Jiro']);
 });
